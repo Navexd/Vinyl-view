44c5cfeeccd61438837b9e8d4039e8cec019dfad
navet <nave.t972@outlook.fr>
2025-12-06 02:53:08 +0100

    reading path

--- frontend/main.js
+++ frontend/main.js
@@ -1,16 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
     const win = new BrowserWindow({
-        fullscreen: true,
-        frame: false,
+        fullscreen: true,          // mode économiseur d’écran
+        frame: false,              // pas de barre de fenêtre
         backgroundColor: '#000000',
         webPreferences: {
             nodeIntegration: true
         }
-      });
+    });
 
-      win.loadFile('renderer/index.html')
+    // Charge ton fichier index.html dans frontend/renderer
+    win.loadFile(path.join(__dirname, 'renderer', 'index.html'));
 }
-app.whenReady().then(createWindow)
+
+app.whenReady().then(createWindow);
 
